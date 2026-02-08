345bd736b1966bc8f88b2343a428983105e33fc5
Yuri Guajardo <yuri@einsmart.cl>
2026-02-08 12:34:16 -0300

    Revert to res.status(200).end() for Vercel compatibility

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
   // Handle preflight immediately
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+    return res.status(200).end();
   }
 
   next();
