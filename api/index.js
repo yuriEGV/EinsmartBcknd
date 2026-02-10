6d6731c84c3af223b57eb45454ea363b1efba1fc
Yuri Guajardo <yuri@einsmart.cl>
2026-02-10 12:57:59 -0300

    debug: catch import errors in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,15 @@
-import app from '../src/server.js';
-export default app;
+export default async (req, res) => {
+    try {
+        const serverModule = await import('../src/server.js');
+        const app = serverModule.default;
+        return app(req, res);
+    } catch (err) {
+        console.error('CRASH_ON_IMPORT:', err);
+        res.status(500).json({
+            error: 'CRASH_ON_IMPORT',
+            message: err.message,
+            stack: err.stack,
+            name: err.name
+        });
+    }
+};
