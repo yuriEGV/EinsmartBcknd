68bfdc21a9839fccf946c80ea4f84e00d2e00ef6
Yuri Guajardo <yuri@einsmart.cl>
2026-02-10 12:39:47 -0300

    debug: test minimal express app on Vercel

--- api/index.js
+++ api/index.js
@@ -1,2 +1,4 @@
-import app from '../src/server.js';
+import express from 'express';
+const app = express();
+app.get('/health', (req, res) => res.status(200).send('OK from api/index.js'));
 export default app;
